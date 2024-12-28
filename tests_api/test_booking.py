c398dde7d2f02c4c4c66353e8f55b3bbd51e2c3e
AlexSimnov <102994784+AlexSimnov@users.noreply.github.com>
2024-12-28 15:34:43 +0300

    add test_negative_post_booking

--- tests_api/test_booking.py
+++ tests_api/test_booking.py
@@ -11,7 +11,7 @@ class TestBooking:
 
         assert create_booking.status_code == 200, 'Ошибка при создании брони'
 
-        booking_id = create_booking.json().get('booking_id')
+        booking_id = create_booking.json().get('bookingid')
 
         assert booking_id is not None, 'Идентификатор брони не обнаружен'
 
@@ -37,49 +37,15 @@ class TestBooking:
             'Бронь не удалась'
         )
 
-    def test_patch_booking(self, auth_create_session, booking_data):
-        booking_id = auth_create_session.get('booking_id')
-
-        patch_booking = auth_create_session.patch(
-            f'{BASE_URL}/booking/{booking_id}',
-            json=booking_data
-        )
-
-        assert patch_booking.status_code == HTTPStatus.OK
-
-        assert patch_booking.json()['booking_id']['firstname'] == booking_data['firstname']
-
-        get_booking = auth_create_session.get(
-            f'{BASE_URL}/booking/{booking_id}'
-        )
-
-        assert get_booking.status_code == HTTPStatus.OK
-
-    def test_put_booking(self, auth_create_session, booking_data):
-        booking_id = auth_create_session.get('booking_id')
-
-        put_booking = auth_create_session.put(
-            f'{BASE_URL}/booking/{booking_id}',
-            json={'lastame': booking_data['lastname']}
-        )
-
-        assert put_booking.status_code == HTTPStatus.OK
-
-        assert put_booking.json()['booking']['lastname'] == booking_data['lastname']
-
-        get_booking = auth_create_session.get(
-            f'{BASE_URL}/booking/{booking_id}'
-        )
-
-        assert get_booking.status_code == HTTPStatus.OK
-
     def test_negative_post_booking(self, auth_session, booking_data):
+
+        booking_data.update({'totalprice': 'total'})
 
         post_booking = auth_session.post(
             f'{BASE_URL}/booking',
-            json=booking_data.update({'totalprice': 'total'})
-        )
+            json=booking_data)
 
-        print(booking_data['totalprice'])
+        assert post_booking.status_code == HTTPStatus.BAD_REQUEST
 
-        assert post_booking.json()['booking']['totalprice'] == booking_data['totalprice']
+    def test_put_booking(self, auth_session, booking_data):
+        pass
